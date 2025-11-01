0cb56560d70881f03aff9931783a17d14225cc72
Anuroopa <anurooopadevds@gmail.com>
2025-11-01 16:49:17 +0530

    Added new dataset for stimulation

--- stimulation.py
+++ stimulation.py
@@ -13,10 +13,8 @@ with col2:
 
 @st.cache_data
 def load_data():
-    stats = pd.read_csv("stats.csv", index_col = 0)
-    data = pd.read_csv("data.csv", index_col = 0)
     type_chart = pd.read_csv("Pokemon_Type_Chart.csv", index_col = 0)
-    details = stats.merge(data)
+    details = pd.read_csv("data_for_stimulation.csv", index_col = 0)
     return details, type_chart
     
 
@@ -26,25 +24,24 @@ def do_all_together(details, type_chart):
     with st.form(key = "pokemon"):
         name = st.text_input("Enter name of the Pokemon: ")
         name = name.capitalize()
-        level = st.slider("Select Level of the Pokemon", 1, 100, 50)
+        #level = st.slider("Select Level of the Pokemon", 1, 100, 50)
         submitted = st.form_submit_button("Submit")
         
     if submitted:
-        def stimulator(details, type_chart, name, level=50):
         
+        def stimulator(details, type_chart, name):
+            """
+            Parameters: 
+            details - pd.DataFrame containg pokemon details scraped from bulbapedia
+            type_chart - pd.DataFrame of pokemon damage type chart with attacking type 
+            as index and defensing type as column names
+            name - str of name of the particular pokemon you want to stimulate
+            Returns:
+            pd.DataFrame with columns name, outcome
+            """
+            
             details = details.copy()
-            
-            details["hp"] = np.floor(((2 * details["hp"] + 31) * level) / 100) + level + 10
-            details["attack"] = np.floor(((2 * details["attack"] + 31) * level) / 100) + 5
-            details["defense"] = np.floor(((2 * details["defense"] + 31) * level) / 100) + 5
-            details["special_attack"] = np.floor(((2 * details["special_attack"] + 31) * level) / 100) + 5
-            details["special_defense"] = np.floor(((2 * details["special_defense"] + 31) * level) / 100) + 5
-            details["speed"] = np.floor(((2 * details["speed"] + 31) * level) / 100) + 5
-            details[["type1", "type2"]] = details["type"].str.split(" / ", expand = True)
-            details["type2"] = details["type2"].fillna("Nil")
-            
-            details["main_attack"] = np.maximum(details["attack"], details["special_attack"])
-            
+        
             pokemon = details[details["name"].str.contains(name, case = False)]
             if len(pokemon) < 1:
                 print(f"The given name: {name} is not a name of available pokemon")
@@ -53,7 +50,7 @@ def do_all_together(details, type_chart):
             attack = pokemon["attack"].iloc[0]
             special_attack = pokemon["special_attack"].iloc[0]
             details["main_defense"] = np.where(attack >= special_attack, details["defense"], details["special_defense"])
-        
+            number = pokemon["number"].astype(str).str.zfill(3).iloc[0]
             speed = pokemon["speed"].iloc[0]
             defense = pokemon["defense"].iloc[0]
             special_defense = pokemon["special_defense"].iloc[0]
@@ -61,11 +58,6 @@ def do_all_together(details, type_chart):
             type1 = pokemon["type1"].iloc[0]
             type2 = pokemon["type2"].iloc[0]
             hp = pokemon["hp"].iloc[0]
-            
-            if pokemon["number"].iloc[0].startswith("#0"):
-                number = pokemon["number"].iloc[0].replace("#0", "")
-            else:
-                number = pokemon["number"].iloc[0].replace("#", "")
             details["pokemon_defense"] = np.where(details["attack"] >= details["special_attack"],defense, special_defense)
         
                 
@@ -101,6 +93,7 @@ def do_all_together(details, type_chart):
                                                             details["others_type2_offense"])
             
             power = 80
+            level = 50
             
             details["damage_dealt_pokemon"] = (((2*level/5+2)*power*details["A/D_pokemon"])/50+2)*details["pokemon_type_offense"]
             details["damage_dealt_others"] = (((2*level/5+2)*power*details["A/D_others"])/50+2)*details["others_type_offense"]
@@ -115,11 +108,11 @@ def do_all_together(details, type_chart):
             details["outcome"] = np.where(details["turns_to_win_pokemon"] < details["turns_to_win_others"], "win",
                                           np.where(details["turns_to_win_pokemon"] > details["turns_to_win_others"], "lose",
                                                    np.where(details["chance"] == 1, "win", "lose")))
-        
+            
             return details.loc[:, ["name", "outcome"]], number
 
         try:
-            result, number = stimulator(details, type_chart, name, level)
+            result, number = stimulator(details, type_chart, name)
             if number == None:
                 st.error(f"Name of the Pokemon {name} not found please check the spelling")
             else:
@@ -136,10 +129,10 @@ def do_one_vs_one(details, type_chart):
     with st.form(key="one"):
         for_ = st.text_input("Enter the name of Your Pokemon")
         against = st.text_input("Enter the name of the Opponent Pokemon")
-        level = st.slider("Select Level of Both The Pokemon", 1, 100, 50)
+        #level = st.slider("Select Level of Both The Pokemon", 1, 100, 50)
         submitted = st.form_submit_button("Submit")
     if submitted:
-        def one_vs_one(details, type_chart, for_, against, level=50):
+        def one_vs_one(details, type_chart, for_, against):
             """
             Parameters: details - pd.DataFrame() containing necessary details of Pokemon scraped from Bulbapedia
             type_chart: pd.DataFrame() containing Pokemon type chart with offensive typing as row index and defensive a column headings
@@ -149,16 +142,6 @@ def do_one_vs_one(details, type_chart):
             """
             details = details.copy()
             
-            details["hp"] = np.floor(((2 * details["hp"] + 31) * level) / 100) + level + 10
-            details["attack"] = np.floor(((2 * details["attack"] + 31) * level) / 100) + 5
-            details["defense"] = np.floor(((2 * details["defense"] + 31) * level) / 100) + 5
-            details["special_attack"] = np.floor(((2 * details["special_attack"] + 31) * level) / 100) + 5
-            details["special_defense"] = np.floor(((2 * details["special_defense"] + 31) * level) / 100) + 5
-            details["speed"] = np.floor(((2 * details["speed"] + 31) * level) / 100) + 5
-            details[["type1", "type2"]] = details["type"].str.split(" / ", expand = True)
-            details["type2"] = details["type2"].fillna("Nil")
-            
-            details["main_attack"] = np.maximum(details["attack"], details["special_attack"])
             for_ = for_.capitalize()
             against = against.capitalize()
             for_data = details[details["name"] == for_]
@@ -175,10 +158,8 @@ def do_one_vs_one(details, type_chart):
             for_hp = for_data["hp"].iloc[0]
             for_type1 = for_data["type1"].iloc[0]
             for_type2 = for_data["type2"].iloc[0]
-            if for_data["number"].iloc[0].startswith("#0"):
-                for_number = for_data["number"].iloc[0].replace("#0", "")
-            else:
-                for_number = for_data["number"].iloc[0].replace("#", "")
+
+            for_number = for_data["number"].astype(str).str.zfill(3).iloc[0]
             against_attack = against_data["main_attack"].iloc[0]
             against_defense = np.where(for_data["attack"] >= for_data["special_attack"],
                                       against_data["defense"].iloc[0], against_data["special_defense"].iloc[0])
@@ -186,10 +167,7 @@ def do_one_vs_one(details, type_chart):
             against_hp = against_data["hp"].iloc[0]
             against_type1 = against_data["type1"].iloc[0]
             against_type2 = against_data["type2"].iloc[0]
-            if against_data["number"].iloc[0].startswith("#0"):
-                against_number = against_data["number"].iloc[0].replace("#0", "")
-            else:
-                against_number = against_data["number"].iloc[0].replace("#", "")
+            against_number = against_data["number"].astype(str).str.zfill(3).iloc[0]
         
             for_ad = for_attack/against_defense
             against_ad = against_attack/for_defense
@@ -230,7 +208,7 @@ def do_one_vs_one(details, type_chart):
                 return "lose", for_number, against_number
 
         try:
-            result, for_number, against_number = one_vs_one(details, type_chart, for_, against, level)
+            result, for_number, against_number = one_vs_one(details, type_chart, for_, against)
             if result == None: 
                 st.error("Name of the Pokemon not found please check the spelling")
             else:
